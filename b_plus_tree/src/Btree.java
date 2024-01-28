27577c15760371af97037b392374ac617638556a
ferreshteh <fereshteh.khademi1381@gmail.com>
2024-01-28 14:30:20 +0330

    adding node class

---
+++ b_plus_tree/src/Btree.java
@@ -0,0 +1,12 @@
+class BPlusTree<K extends Comparable<K>, T> {
+
+    public Node<K, T> root;
+    public static final int D = 2;
+
+/**
+ * TODO Search the value for a specific key
+ *
+ * @param key
+ * @return value
+ **/
+}
