1da7018de2eafef0dc28e2bd8c992288a8d7bb7d
ferreshteh <fereshteh.khademi1381@gmail.com>
2024-01-30 09:47:53 +0330

    creating database class

--- database/Table.java
+++ database/Table.java
@@ -4,10 +4,12 @@ import bPlusTree.*;
 
 public class Table<T> {
     private Column[] columns;
+    private Row[] rows;
     private BPlusTree<Integer,T> table;
 
     public Table(int number) {
         this.columns = new Column[number];
+        this.rows=new Row[number];
         this.table=new BPlusTree<Integer,T>();
     }
 
